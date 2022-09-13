084f2232ac20dc73979345de23c7a77e8a331886
adrian_segura <adrianseor.99@gmail.com>
2022-09-13 15:56:44 +0200

    Add validator tests

--- pkg/workflows/topological_sorting_test.go
+++ pkg/workflows/topological_sorting_test.go
@@ -14,9 +14,9 @@ func TestTopologicalGroupedSort(t *testing.T) {
 		workflowFile string
 		want [][]string
 	}{
-		{"./../../data/hector/workflow_tests/workflow_test_1.json", [][]string{{"A"}, {"C"}, {"B"}, {"D"}}},
-		{"./../../data/hector/workflow_tests/workflow_test_2.json", [][]string{{"4", "5"}, {"0", "2"}, {"3"}, {"1"}}}, // Based on https://www.geeksforgeeks.org/topological-sorting/ DAG
-		{"./../../data/hector/workflow_tests/workflow_test_3.json", [][]string{{"3", "5", "7"}, {"8", "11"}, {"2", "9", "10"}}}, // Based on https://upload.wikimedia.org/wikipedia/commons/0/08/Directed_acyclic_graph.png
+		{"./../../data/hector/test_topological_sort/test_topological_sort_1.json", [][]string{{"A"}, {"C"}, {"B"}, {"D"}}},
+		{"./../../data/hector/test_topological_sort/test_topological_sort_2.json", [][]string{{"4", "5"}, {"0", "2"}, {"3"}, {"1"}}}, // Based on https://www.geeksforgeeks.org/topological-sorting/ DAG
+		{"./../../data/hector/test_topological_sort/test_topological_sort_3.json", [][]string{{"3", "5", "7"}, {"8", "11"}, {"2", "9", "10"}}}, // Based on https://upload.wikimedia.org/wikipedia/commons/0/08/Directed_acyclic_graph.png
 	}
 
 	for _, tt := range tests {
