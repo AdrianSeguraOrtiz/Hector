44c9421000d8ee111c813b3b57bd5e7e2b227161
adrian_segura <adrianseor.99@gmail.com>
2022-11-10 15:49:07 +0100

    Add 2/5 test funtions

--- pkg/executors/nomad/nomad_test.go
+++ pkg/executors/nomad/nomad_test.go
@@ -1,13 +1,179 @@
 package nomad
 
 import (
+	"dag/hector/golang/module/pkg"
 	"dag/hector/golang/module/pkg/definitions"
 	"dag/hector/golang/module/pkg/jobs"
 	"dag/hector/golang/module/pkg/results"
+	"reflect"
 	"strconv"
 	"strings"
 	"testing"
+
+	"github.com/hashicorp/nomad/api"
 )
+
+func TestArgumentsToSlice(t *testing.T) {
+	var tests = []struct {
+		arguments *[]definitions.Parameter
+		array     []string
+	}{
+		{
+			arguments: &[]definitions.Parameter{
+				{
+					Name:  "name-1",
+					Value: "value-1",
+				},
+			},
+			array: []string{"--name-1", "value-1"},
+		},
+		{
+			arguments: &[]definitions.Parameter{
+				{
+					Name:  "name-2",
+					Value: 2,
+				},
+			},
+			array: []string{"--name-2", "2"},
+		},
+		{
+			arguments: &[]definitions.Parameter{
+				{
+					Name:  "name-3",
+					Value: true,
+				},
+			},
+			array: []string{"--name-3", "true"},
+		},
+		{
+			arguments: &[]definitions.Parameter{
+				{
+					Name:  "name-1",
+					Value: "value-1",
+				},
+				{
+					Name:  "name-2",
+					Value: 2,
+				},
+				{
+					Name:  "name-3",
+					Value: true,
+				},
+			},
+			array: []string{"--name-1", "value-1", "--name-2", "2", "--name-3", "true"},
+		},
+	}
+
+	for i, tt := range tests {
+
+		testname := "test_" + strconv.Itoa(i)
+		t.Run(testname, func(t *testing.T) {
+			args := argumentsToSlice(tt.arguments)
+			if !reflect.DeepEqual(args, tt.array) {
+				t.Error("got ", args, ", want ", tt.array)
+			}
+		})
+	}
+
+}
+
+func TestBuildJob(t *testing.T) {
+	var tests = []struct {
+		job      *jobs.Job
+		nomadJob *api.Job
+	}{
+		{
+			job: &jobs.Job{
+				Id:    "Job-Id-1",
+				Name:  "Job Name 1",
+				Image: "image-name-1",
+			},
+			nomadJob: &api.Job{
+				ID:          pkg.Ptr("Job-Id-1"),
+				Name:        pkg.Ptr("Job Name 1"),
+				Type:        pkg.Ptr("batch"),
+				Datacenters: []string{"dc1"},
+				TaskGroups: []*api.TaskGroup{
+					{
+						Name: pkg.Ptr("Task-Group-Job-Id-1"),
+						Tasks: []*api.Task{
+							{
+								Name:   "Task-Job-Id-1",
+								Driver: "docker",
+								Config: map[string]interface{}{
+									"image": "image-name-1",
+									"args":  []string{},
+								},
+								RestartPolicy: &api.RestartPolicy{Attempts: pkg.Ptr(0)},
+							},
+						},
+						RestartPolicy: &api.RestartPolicy{Attempts: pkg.Ptr(0)},
+					},
+				},
+				Reschedule: &api.ReschedulePolicy{Attempts: pkg.Ptr(0)},
+			},
+		},
+		{
+			job: &jobs.Job{
+				Id:    "Job-Id-2",
+				Name:  "Job Name 2",
+				Image: "image-name-2",
+				Arguments: []definitions.Parameter{
+					{
+						Name:  "name-1",
+						Value: "value-1",
+					},
+					{
+						Name:  "name-2",
+						Value: 2,
+					},
+					{
+						Name:  "name-3",
+						Value: true,
+					},
+				},
+			},
+			nomadJob: &api.Job{
+				ID:          pkg.Ptr("Job-Id-2"),
+				Name:        pkg.Ptr("Job Name 2"),
+				Type:        pkg.Ptr("batch"),
+				Datacenters: []string{"dc1"},
+				TaskGroups: []*api.TaskGroup{
+					{
+						Name: pkg.Ptr("Task-Group-Job-Id-2"),
+						Tasks: []*api.Task{
+							{
+								Name:   "Task-Job-Id-2",
+								Driver: "docker",
+								Config: map[string]interface{}{
+									"image": "image-name-2",
+									"args":  []string{"--name-1", "value-1", "--name-2", "2", "--name-3", "true"},
+								},
+								RestartPolicy: &api.RestartPolicy{Attempts: pkg.Ptr(0)},
+							},
+						},
+						RestartPolicy: &api.RestartPolicy{Attempts: pkg.Ptr(0)},
+					},
+				},
+				Reschedule: &api.ReschedulePolicy{Attempts: pkg.Ptr(0)},
+			},
+		},
+	}
+
+	for i, tt := range tests {
+
+		testname := "test_" + strconv.Itoa(i)
+		t.Run(testname, func(t *testing.T) {
+			taskName := "Task-" + tt.job.Id
+			taskGroupName := "Task-Group-" + tt.job.Id
+			nomadJob := buildJob(tt.job, taskName, taskGroupName)
+
+			if equal, message := pkg.DeepValueEqual(*nomadJob, *tt.nomadJob, true); !equal {
+				t.Error(message)
+			}
+		})
+	}
+}
 
 func TestExecuteJob(t *testing.T) {
 	var tests = []struct {
